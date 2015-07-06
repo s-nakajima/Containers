6d7fc0f99d6cedd3eed72d158b8b58d858698744
s-nakajima <nakajimashouhei@gmail.com>
2015-07-07 00:15:26 +0900

    Add containers recodes and containers_pages recodes

--- Config/Migration/1434983278_init.php
+++ Config/Migration/1434983278_init.php
@@ -71,6 +71,69 @@ e.g.) ルーム管理者、またはそれに準ずるユーザ: ルーム管理
 	);
 
 /**
+ * Records keyed by model name.
+ *
+ * @var array $records
+ */
+	public $records = array(
+		'Container' => array(
+			array(
+				'id' => '1',
+				'type' => '1',
+			),
+			array(
+				'id' => '2',
+				'type' => '2',
+			),
+			array (
+				'id' => '3',
+				'type' => '3',
+			),
+			array(
+				'id' => '4',
+				'type' => '4',
+			),
+			array(
+				'id' => '5',
+				'type' => '5',
+			),
+		),
+
+		'ContainersPage' => array(
+			array(
+				'id' => '1',
+				'page_id' => '1',
+				'container_id' => '1',
+				'is_published' => true,
+			),
+			array(
+				'id' => '2',
+				'page_id' => '1',
+				'container_id' => '2',
+				'is_published' => true,
+			),
+			array(
+				'id' => '3',
+				'page_id' => '1',
+				'container_id' => '3',
+				'is_published' => true,
+			),
+			array(
+				'id' => '4',
+				'page_id' => '1',
+				'container_id' => '4',
+				'is_published' => true,
+			),
+			array(
+				'id' => '5',
+				'page_id' => '1',
+				'container_id' => '5',
+				'is_published' => true,
+			),
+		),
+	);
+
+/**
  * Before migration callback
  *
  * @param string $direction Direction of migration process (up or down)
@@ -87,6 +150,37 @@ e.g.) ルーム管理者、またはそれに準ずるユーザ: ルーム管理
  * @return bool Should process continue
  */
 	public function after($direction) {
+		if ($direction === 'down') {
+			return true;
+		}
+
+		foreach ($this->records as $model => $records) {
+			if (!$this->updateRecords($model, $records)) {
+				return false;
+			}
+		}
+
 		return true;
 	}
+
+/**
+ * Update model records
+ *
+ * @param string $model model name to update
+ * @param string $records records to be stored
+ * @param string $scope ?
+ * @return bool Should process continue
+ */
+	public function updateRecords($model, $records, $scope = null) {
+		$Model = $this->generateModel($model);
+		foreach ($records as $record) {
+			$Model->create();
+			if (!$Model->save($record, false)) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
 }
