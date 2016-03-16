960153e81eb28d3329301fc1d4c1f498eacc5d4d
s-nakajima <nakajimashouhei@gmail.com>
2016-03-16 11:45:28 +0900

    レイアウトを設定したかどうかのフラグを追加

--- Config/Migration/1434983278_init.php
+++ Config/Migration/1434983278_init.php
@@ -33,8 +33,7 @@ class Init extends CakeMigration {
 			'create_table' => array(
 				'containers' => array(
 					'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary'),
-					'type' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false, 'comment' => 'Type of the container.
-1:Header, 2:Major, 3:Main, 4:Minor, 5:Footer'),
+					'type' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false, 'comment' => 'Type of the container.  1:Header, 2:Major, 3:Main, 4:Minor, 5:Footer'),
 					'created_user' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false),
 					'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
 					'modified_user' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false),
@@ -48,10 +47,8 @@ class Init extends CakeMigration {
 					'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary'),
 					'page_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false),
 					'container_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false),
-					'is_published' => array('type' => 'boolean', 'null' => true, 'default' => null, 'comment' => '一般以下のパートが閲覧可能かどうか。
-
-ルーム配下ならルーム管理者、またはそれに準ずるユーザ(room_parts.edit_page, room_parts.create_page 双方が true のユーザ)はこの値に関わらず閲覧できる。
-e.g.) ルーム管理者、またはそれに準ずるユーザ: ルーム管理者、編集長'),
+					'is_published' => array('type' => 'boolean', 'null' => true, 'default' => null, 'comment' => '一般以下のパートが閲覧可能かどうか。ルーム配下ならルーム管理者、またはそれに準ずるユーザはこの値に関わらず閲覧できる。  e.g.) ルーム管理者、またはそれに準ずるユーザ: ルーム管理者、編集長'),
+					'is_configured' => array('type' => 'boolean', 'null' => false, 'default' => false, 'comment' => '設定したかどうか'),
 					'created_user' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false),
 					'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
 					'modified_user' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false),
