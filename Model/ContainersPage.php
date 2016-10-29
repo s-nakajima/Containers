ed5d8c67f80181b3b3f7b6cf6b6b5f421879d9bb
s-nakajima <nakajimashouhei@gmail.com>
2016-10-29 17:46:00 +0900

    ボックスの切り替え機能追加による修正 https://github.com/NetCommons3/NetCommons3/issues/726

--- Model/ContainersPage.php
+++ Model/ContainersPage.php
@@ -2,6 +2,8 @@
 /**
  * ContainersPage Model
  *
+ * ※v3.0.2で不要
+ *
  * @property Page $Page
  * @property Container $Container
  *
@@ -11,137 +13,15 @@
  * @license http://www.netcommons.org/license.txt NetCommons License
  */
 
-App::uses('PagesAppModel', 'Pages.Model');
+// @codeCoverageIgnoreStart
+App::uses('PageContainer', 'Pages.Model');
+// @codeCoverageIgnoreEnd
 
 /**
  * Summary for ContainersPage Model
+ *
+ * @codeCoverageIgnore
  */
 class ContainersPage extends PagesAppModel {
 
-/**
- * Validation rules
- *
- * @var array
- */
-	public $validate = array();
-
-	//The Associations below have been created with all possible keys, those that are not needed can be removed
-
-/**
- * belongsTo associations
- *
- * @var array
- */
-	public $belongsTo = array(
-		'Page' => array(
-			'className' => 'Pages.Page',
-			'foreignKey' => 'page_id',
-			'conditions' => '',
-			'fields' => '',
-			'order' => ''
-		),
-		'Container' => array(
-			'className' => 'Containers.Container',
-			'foreignKey' => 'container_id',
-			'conditions' => '',
-			'fields' => '',
-			'order' => ''
-		)
-	);
-
-/**
- * Called during validation operations, before validation. Please note that custom
- * validation rules can be defined in $validate.
- *
- * @param array $options Options passed from Model::save().
- * @return bool True if validate operation should continue, false to abort
- * @link http://book.cakephp.org/2.0/en/models/callback-methods.html#beforevalidate
- * @see Model::save()
- */
-	public function beforeValidate($options = array()) {
-		$this->validate = Hash::merge(array(
-			'page_id' => array(
-				'numeric' => array(
-					'rule' => array('numeric'),
-					'message' => __d('net_commons', 'Invalid request.'),
-				),
-			),
-			'container_id' => array(
-				'numeric' => array(
-					'rule' => array('numeric'),
-					'message' => __d('net_commons', 'Invalid request.'),
-				),
-			),
-			'is_published' => array(
-				'boolean' => array(
-					'rule' => array('boolean'),
-					'message' => __d('net_commons', 'Invalid request.'),
-				),
-			),
-			'is_configured' => array(
-				'boolean' => array(
-					'rule' => array('boolean'),
-					'message' => __d('net_commons', 'Invalid request.'),
-				),
-			),
-		), $this->validate);
-
-		return parent::beforeValidate($options);
-	}
-
-/**
- * Save page each association model
- *
- * @param array $data request data
- * @throws InternalErrorException
- * @return mixed On success Model::$data if its not empty or true, false on failure
- */
-	public function saveContainersPage($data) {
-		//トランザクションBegin
-		$this->begin();
-
-		if (! $this->validateMany($data['ContainersPage'])) {
-			return false;
-		}
-		try {
-			if (! $this->saveMany($data['ContainersPage'], ['validate' => false])) {
-				throw new InternalErrorException(__d('net_commons', 'Internal Server Error'));
-			}
-
-			if (Hash::get($data, 'ChildPage.id')) {
-				$childPageId = explode(',', Hash::get($data, 'ChildPage.id', ''));
-
-				$containerPages = Hash::get($data, 'ContainersPage');
-				$containerTypes = array(
-					Container::TYPE_HEADER, Container::TYPE_MAJOR, Container::TYPE_MINOR, Container::TYPE_FOOTER
-				);
-				foreach ($containerTypes as $containerType) {
-					$updated = array(
-						'ContainersPage.is_published' => Hash::get(
-							$containerPages, $containerType . '.ContainersPage.is_published', true
-						),
-					);
-					$conditions = array(
-						'ContainersPage.is_configured' => false,
-						'ContainersPage.page_id' => $childPageId,
-						'ContainersPage.container_id' => Hash::get(
-							$containerPages, $containerType . '.ContainersPage.container_id'
-						),
-					);
-
-					$result = $this->updateAll($updated, $conditions);
-					if (! $result) {
-						throw new InternalErrorException(__d('net_commons', 'Internal Server Error'));
-					}
-				}
-			}
-
-			$this->commit();
-
-		} catch (Exception $ex) {
-			$this->rollback($ex);
-		}
-
-		return true;
-	}
 }
