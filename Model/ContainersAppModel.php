3fe780ab8c3ee84c552581dce6f699953480fbd0
s-nakajima <nakajimashouhei@gmail.com>
2016-10-29 17:39:06 +0900

    間違ってコミットしてしまったので、一回元に戻す。

--- Model/ContainersAppModel.php
+++ Model/ContainersAppModel.php
@@ -8,16 +8,13 @@
  * @license http://www.netcommons.org/license.txt NetCommons License
  */
 
-// @codeCoverageIgnoreStart
 App::uses('AppModel', 'Model');
-// @codeCoverageIgnoreEnd
 
 /**
  * ContainersApp Model
  *
  * @author Kohei Teraguchi <kteraguchi@commonsnet.org>
  * @package NetCommons\Containers\Model
- * @codeCoverageIgnore
  */
 class ContainersAppModel extends AppModel {
 
